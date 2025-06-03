adce4b93c58960883840864fb03cf5c69b089792
donywahyur <donyramadhan3011@gmail.com>
2025-06-03 11:30:00 +0800

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import fs from "fs";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0", // Bind to all network interfaces inside the container
+    port: 5173, // Ensure the port matches your Docker setup
+    strictPort: true, // Fail immediately if the port is unavailable
+    hmr: {
+      protocol: "wss", // Use WebSockets over HTTPS
+      host: "dynodhan.xyz", // Use the domain where the browser is running
+    },
+  },
+});
